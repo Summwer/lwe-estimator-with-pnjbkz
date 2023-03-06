8e32091f09294c8d3f69f91c7c51bd5d781b0f11
Summwer <15091002@buaa.edu.cn>
2023-03-06 10:48:21 +0800

    udpate bssa

--- enumbs-estimation-cpp/lwechal-bssa-est-sec.sh
+++ enumbs-estimation-cpp/lwechal-bssa-est-sec.sh
@@ -1,3 +1,4 @@
 export LD_LIBRARY_PATH="./:/usr/local/lib/:/usr/lib/x86_64-linux-gnu/"
+mkdir bssa_result
 g++ -O3 -funroll-loops -o lwechal-bssa-est-sec ./NIST-round3/lwechal-bssa-est-sec.cpp -L. -pthread -lfplll -lgmp -lmpfr -lest
-./lwechal-bssa-est-sec
+./lwechal-bssa-est-sec | tee bssa_result/lwechal-bssa-est-sec4.log
